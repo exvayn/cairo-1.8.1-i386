a119fc850fb0100cb74d4cc9d7398f2b49e26737
exvayn <bairbol@gmail.com>
2016-06-04 17:17:06 +0800

    i386 linux

--- pkg-config.rb
+++ pkg-config.rb
@@ -210,6 +210,7 @@ class PackageConfig
                     "/usr/lib64/pkgconfig",
                     "/usr/lib/pkgconfig",
                     "/usr/X11/lib/pkgconfig/",
+                    "/usr/lib/i386-linux-gnu/pkgconfig",
                     "/usr/share/pkgconfig"].join(separator)
     libdir = ENV["PKG_CONFIG_LIBDIR"]
     default_path = [libdir, default_path].join(separator) if libdir
